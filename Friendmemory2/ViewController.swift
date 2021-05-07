0479065f92fa88197e5141ab8a27cf29b9590fd9
nokkaa <noka.mirumuru@icloud.com>
2021-05-07 20:18:22 +0900

    Initial Commit

---
+++ Friendmemory2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Friendmemory2
+//
+//  Created by nodoka on 2021/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
